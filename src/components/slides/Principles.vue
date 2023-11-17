2650b72c76a561b447f87d1961edbe6997128e5d
jonas <nayow@hotmail.fr>
2023-11-17 14:31:44 +0100

    add: principles

--- src/components/slides/Principles.vue
+++ src/components/slides/Principles.vue
@@ -1,6 +1,22 @@
 <template lang="pug">
-h1 Principles
-div Heavily inspired by the principles of Burning Man, we have created our own three Principles: Sharing and caring: At our festival, we embrace the spirit of respect and love, fostering a close-knit community where kindness, thoughtfulness and generosity flow freely among all participants. Creating and Cooperating: In a symphony of collaborative synergies, we unite our creativity and skills, where the fusion of each individual brilliance contributes to the greater good. Exploring and restoring: Shedding the constraints of our everyday life, we encourage free expression and inner exploration, while nurturing a culture of mindfulness to ensure the well-being of both mind and body throughout our journey together.
+.flex.flex-col.h-full.w-full
+  img.absolute(class="min-w-[150%]" src="@/assets/images/8/theme.svg")
+  
+  .relative.p-6.flex.flex-co
+    .title As long as you follow our principles
+    .flex.flex-col.gap-3.text-sm.text-justify.p-4(class="w-2/3 rounded rounded-[10px]" style="background: rgb(230, 230, 230, 0.75)")
+      div Heavily inspired by the principles of Burning Man, we have created our own three Principles: 
+      div
+        span.font-semibold Sharing and caring :&nbsp;
+        span at our festival, we embrace the spirit of respect and love, fostering a close-knit community where kindness, thoughtfulness and generosity flow freely among all participants.
+      div
+        span.font-semibold Creating and Cooperating :&nbsp;
+        span in a symphony of collaborative synergies, we unite our creativity and skills, where the fusion of each individual brilliance contributes to the greater good.
+      div
+        span.font-semibold Exploring and restoring :&nbsp;
+        span shedding the constraints of our everyday life, we encourage free expression and inner exploration, while nurturing a culture of mindfulness to ensure the well-being of both mind and body throughout our journey together.
+
+  arrow-down
 </template>
 
 <script>
